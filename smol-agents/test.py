a10b6c874e519bdf1acab3a2f869fadd1e8561dc
Iker Macaya Faber <iker@macaya.dev>
2025-01-14 11:41:44 +0100

    Superbowl odds for every team

--- smol-agents/test.py
+++ smol-agents/test.py
@@ -1,9 +1,13 @@
 from smolagents import CodeAgent, DuckDuckGoSearchTool, LiteLLMModel
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
 
 model = LiteLLMModel(model_id="gemini/gemini-1.5-flash",
-                     api_key=(geminiApiKey))
+                     api_key=(api_key))
 
 agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=model)
 
-agent.run("What are the next NFL playoff matchs?")
+agent.run("Give the probabilities for each team alive on the NFL to win the Superbowl?")
